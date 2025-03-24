d6c743b7a2607666bbfa54672806fd5023101359
unseo jung <jsally2004@gmail.com>
2025-03-24 03:26:09 +0000

    구구단

---
+++ test.py
@@ -0,0 +1,11 @@
+# number = int(input("구구단을 출력할 숫자를 입력하세요 (예: 2): "))
+# print(f"{nunmer}단")
+s_number = input("구구단을 출력할 숫자를 입력하세요 (예: 2): ")
+print(f"{s_number}단: ")
+# print(number, "단:")
+# print(number + "단:")
+# print(number + "단, " + number + "단, " + number +"단")
+# print(f"{number}단: ")
+number = int(s_number)
+result =  number * 2
+print(result)
